dd2a4165489f5ae7c462a763061ff067844c3e4a
pablo <pablovicenteroura2005@gmail.com>
2024-10-04 16:14:20 +0200

    commitPlantilla

--- HundirLaFlota/src/main/java/com/project/Controller.java
+++ HundirLaFlota/src/main/java/com/project/Controller.java
@@ -2,12 +2,29 @@ package com.project;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 
 public class Controller implements Initializable {
+
+    @FXML
+    private static TextField nameField;
+
+    @FXML
+    private static TextField ipField;
+
+    @FXML
+    private static TextField portField;
+
+    @FXML
+    private static Button cancelButton;
+
+    @FXML
+    private static Button acceptButton;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
