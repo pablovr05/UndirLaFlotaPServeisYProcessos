a00ce855f70537de093974b427d73490edd227e3
gbiagi <geanfrancobiagi@hotmail.com>
2024-10-04 15:58:43 +0200

    plantilla mvn

---
+++ HundirLaFlota/src/main/java/com/project/Controller.java
@@ -0,0 +1,18 @@
+package com.project;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+
+
+}
