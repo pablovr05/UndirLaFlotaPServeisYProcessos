7dc70e7ad8086788f111af30c54d66eb071b6176
gbiagi <geanfrancobiagi@hotmail.com>
2024-10-30 17:51:40 +0100

    Add extra ships

--- src/main/java/com/client/ControllerConnect.java
+++ src/main/java/com/client/ControllerConnect.java
@@ -7,6 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 import org.java_websocket.client.WebSocketClient;
